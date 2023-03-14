a37b4c7153ff13ff0f80cc238efb1ef933c3d71c
alexshalayev <alex.s@nuvolar.eu>
2023-03-14 11:06:59 +0300

    - update test

--- src/test/java/PropertiesValidatorTest.java
+++ src/test/java/PropertiesValidatorTest.java
@@ -1,5 +1,7 @@
 import helper.ProductEnum;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -7,9 +9,16 @@ import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import static helper.StringValueHelper.ICON_TEST_ANSI;
+
 public class PropertiesValidatorTest {
 
     @Test
+    @DisplayName(ICON_TEST_ANSI + "Unit Test: validation of invalid product code")
+    @Description("Unit Test: validation of invalid product code")
+    @Severity(SeverityLevel.MINOR)
+    @Owner("Alex Sh.")
+    @Issue("TEST-#")
     void testInvalidProductCode() {
         CashierSystem cashierSystem = new CashierSystem();
         Map<String, Integer> products = new HashMap<>();
@@ -22,6 +31,11 @@ public class PropertiesValidatorTest {
     }
 
     @Test
+    @DisplayName(ICON_TEST_ANSI + "Unit Test: validation of quantity")
+    @Description("Unit Test: validation of quantity")
+    @Severity(SeverityLevel.MINOR)
+    @Owner("Alex Sh.")
+    @Issue("TEST-#")
     void testCannotConvertQuantity() {
         CashierSystem cashierSystem = new CashierSystem();
         Map<String, Integer> products = new HashMap<>();
@@ -32,6 +46,4 @@ public class PropertiesValidatorTest {
         products.put(validProductCode, -1);
         Assertions.assertThrows(IllegalArgumentException.class, () -> cashierSystem.calculateTotalPrice(products));
     }
-
-    // TODO: don't forget to complete this tests...
 }
